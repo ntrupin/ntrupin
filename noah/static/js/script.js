87cb2cc245b598428a82eca210ed0f15d648b8c9
ntrupin <natrupin@gmail.com>
2022-11-01 17:10:22 -0400

    color

--- noah/static/js/script.js
+++ noah/static/js/script.js
@@ -3,9 +3,11 @@ hljs.highlightAll();
 const colors = [
     "--bs-green", "--bs-blue", "--bs-red", "--bs-purple", "--bs-cyan", "--bs-orange"
 ];
-document.querySelector("#randomColor").addEventListener("click", (e) => {
-    let ds = document.documentElement.getAttribute("style") ?? "";
-    const color = (ds.match(/(?<=\-\-root\-color\: var\().+?(?=\)\;)/g) ?? [""])[0];
-    const set = colors.filter(c => c != color);
-    document.documentElement.style = `--root-color: var(${set[Math.floor(Math.random()*set.length)]});`;
+window.addEventListener('DOMContentLoaded', (_) => {
+    document.querySelector("#randomColor").addEventListener("click", (_) => {
+        let ds = document.documentElement.getAttribute("style") ?? "";
+        const color = (ds.match(/(?<=\-\-root\-color\: var\().+?(?=\)\;)/g) ?? [""])[0];
+        const set = colors.filter(c => c != color);
+        document.documentElement.style = `--root-color: var(${set[Math.floor(Math.random()*set.length)]});`;
+    });
 });

9acf4d42f7e902cad1e533776ba36d3ac24ff298
ntrupin <natrupin@gmail.com>
2022-11-01 12:00:03 -0400

    resume

--- noah/static/js/script.js
+++ noah/static/js/script.js
@@ -1,1 +1,8 @@
 hljs.highlightAll();
+
+const colors = [
+    "--bs-green", "--bs-blue", "--bs-red", "--bs-purple", "--bs-pink"
+];
+document.querySelector("#randomColor").addEventListener("click", (e) => {
+    document.documentElement.style = `--root-color: var(${colors[Math.floor(Math.random()*colors.length)]});`;
+});

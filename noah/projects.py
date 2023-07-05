43696428283250fc5bc3a4011d5ef8031fb58a14
ntrupin <natrupin@gmail.com>
2023-07-05 12:15:31 -0400

    updates

--- noah/projects.py
+++ noah/projects.py
@@ -1,3 +1,4 @@
+import markdown
 from flask import (
     Blueprint, request, render_template, flash, redirect, url_for, g
 )
@@ -35,6 +36,51 @@ def create():
             )
             return redirect(url_for("projects.index"))
     return render_template("projects/create.html")
+
+@bp.route("/<int:id>/update", methods=["GET", "POST"])
+@login_required
+def update(id):
+    project = get_project(id)
+    if request.method == "POST":
+        error = None
+        name = request.form["name"]
+        startdate = request.form["startdate"] if request.form["startdate"] != "" else None
+        enddate = request.form["enddate"]  if request.form["enddate"] != "" else None
+        content = request.form["content"] or "..."
+        public = "public" in request.form
+        if not name:
+            error = "Name is required"
+        if error is not None:
+            flash(error)
+        else:
+            print(g.user["id"])
+            execute(
+                "UPDATE projects SET name = %s, startdate = %s, enddate = %s, content = %s, public = %s"
+                " WHERE ID = %s",
+                args=[name, startdate, enddate, content, public, id]
+            )
+            return redirect(url_for("projects.index"))
+    return render_template("projects/update.html", project=project)
+
+@bp.route("/<int:id>", methods=["GET"])
+def show(id):
+    project = dict(get_project(id))
+    project["content"] = markdown.markdown(project["content"], extensions=["fenced_code", "tables", "nl2br"])
+    return render_template("projects/show.html", project=project)
+
+@bp.route("/<int:id>/delete", methods=["POST"])
+@login_required
+def delete(id):
+    execute("DELETE FROM projects WHERE id = %s", args=[id])
+    return redirect(url_for("projects.index"))
+
+def get_project(id):
+    return execute(
+        "SELECT p.id, name, startdate, enddate, content, public, created, author_id, username"
+        " FROM projects p JOIN users u ON p.author_id = u.id"
+        " WHERE p.id = %s AND public IN %s",
+        args=[id, (True,) if g.user is None else (True, False)]
+    )[0]
 
 def get_projects():
     return execute(

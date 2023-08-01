7856b9528be0237902d7fa5b27ad798eb53aa9c5
ntrupin <natrupin@gmail.com>
2023-08-01 12:41:44 -0400

    updates

--- noah/projects.py
+++ noah/projects.py
@@ -1,6 +1,6 @@
 import markdown
 from flask import (
-    Blueprint, request, render_template, flash, redirect, url_for, g, abort
+    Blueprint, request, render_template, flash, redirect, url_for, g, abort, session
 )
 
 from noah.auth import login_required
@@ -10,7 +10,7 @@ from psycopg2.extras import RealDictRow
 
 bp = Blueprint("projects", __name__, url_prefix="/projects")
 
-FIELDS = ["name", "startdate", "enddate", "brief", "content", "public", "pinned"]
+FIELDS = ["name", "startdate", "enddate", "urlid", "link", "github", "brief", "content", "public", "pinned"]
 
 @bp.route("/")
 def index():
@@ -23,6 +23,9 @@ def create():
     if request.method == "POST":
         error = None
         name = request.form["name"]
+        urlid = request.form["urlid"] if request.form["urlid"] != "" else None
+        link = request.form["link"] if request.form["link"] != "" else None
+        github = request.form["github"] if request.form["github"] != "" else None
         brief = request.form["brief"] if request.form["brief"] != "" else None
         startdate = request.form["startdate"] if request.form["startdate"] != "" else None
         enddate = request.form["enddate"]  if request.form["enddate"] != "" else None
@@ -37,8 +40,8 @@ def create():
             print(g.user["id"])
             execute(
                 f"INSERT INTO projects ({', '.join(FIELDS)}, author_id)"
-                " VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
-                args=[name, startdate, enddate, brief, content, public, pinned, g.user["id"]]
+                " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                args=[name, startdate, enddate, urlid, link, github, brief, content, public, pinned, g.user["id"]]
             )
             return redirect(url_for("projects.index"))
     return render_template("projects/create.html")
@@ -50,6 +53,9 @@ def update(id):
     if request.method == "POST":
         error = None
         name = request.form["name"]
+        urlid = request.form["urlid"] if request.form["urlid"] != "" else None
+        link = request.form["link"] if request.form["link"] != "" else None
+        github = request.form["github"] if request.form["github"] != "" else None
         brief = request.form["brief"] if request.form["brief"] != "" else None
         startdate = request.form["startdate"] if request.form["startdate"] != "" else None
         enddate = request.form["enddate"]  if request.form["enddate"] != "" else None
@@ -64,14 +70,26 @@ def update(id):
             execute(
                 f"UPDATE projects SET {', '.join([f'{x} = %s' for x in FIELDS])}"
                 " WHERE ID = %s",
-                args=[name, startdate, enddate, brief, content, public, pinned, id]
+                args=[name, startdate, enddate, urlid, link, github, brief, content, public, pinned, id]
             )
-            return redirect(url_for("projects.show", id=id))
+            return redirect(url_for("projects.show_id", id=id))
     return render_template("projects/update.html", project=project)
 
 @bp.route("/<int:id>", methods=["GET"])
-def show(id):
+def show_id(id):
     project = dict(get_project(id))
+    if len(project) == 0:
+        abort(404, description="Project not found.")
+    if project["urlid"] != None:
+        session["project"] = project
+        return redirect(url_for("projects.show_name", name=project["urlid"]))
+    project["content"] = markdown.markdown(project["content"], extensions=["fenced_code", "tables", "nl2br", "toc"])
+    return render_template("projects/show.html", project=project)
+
+@bp.route("/<string:name>", methods=["GET"])
+def show_name(name):
+    project = session["project"] or dict(get_project_by_name(name))
+    session["project"] = None
     if len(project) == 0:
         abort(404, description="Project not found.")
     project["content"] = markdown.markdown(project["content"], extensions=["fenced_code", "tables", "nl2br", "toc"])
@@ -89,6 +107,15 @@ def get_project(id):
         " FROM projects p JOIN users u ON p.author_id = u.id"
         " WHERE p.id = %s AND (p.author_id = %s OR public IS TRUE)",
         args=[id, (g.user or {"id": None})["id"]]
+    ) or [RealDictRow()])[0]
+
+
+def get_project_by_name(name):
+    return (execute(
+        f"SELECT p.id, {', '.join(FIELDS)}, created, author_id, username"
+        " FROM projects p JOIN users u ON p.author_id = u.id"
+        " WHERE p.urlid = %s AND (p.author_id = %s OR public IS TRUE)",
+        args=[name, (g.user or {"id": None})["id"]]
     ) or [RealDictRow()])[0]
 
 def get_projects():

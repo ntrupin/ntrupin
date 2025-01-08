abee8be98935193f82a224172a7faceff22391c1
ntrupin <natrupin@gmail.com>
2025-01-08 17:45:29 -0500

    updates

--- noah.py
+++ noah.py
@@ -52,8 +52,8 @@ app.register_blueprint(rss.bp)
 
 @app.errorhandler(403)
 def forbidden(e):
-    return render_template("403.html"), 403
+    return render_template("403.html", omitHeader=True), 403
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template("404.html", omitHeader=True), 404

c3e4cb74d8fccf985b0815979af4a81eb6d82dff
ntrupin <natrupin@gmail.com>
2026-08-22 19:15:44 -0400

    update

--- noah.py
+++ noah.py
@@ -220,8 +220,8 @@ app.register_blueprint(reading_bp)
 from server.writing import bp as writing_bp, show_canonical
 app.register_blueprint(writing_bp)
 
-from server.projects import bp as projects_bp
-app.register_blueprint(projects_bp)
+# from server.projects import bp as projects_bp
+# app.register_blueprint(projects_bp)
 
 from server.admin import bp as admin_bp
 app.register_blueprint(admin_bp)
--- server/meta.py
+++ server/meta.py
@@ -33,7 +33,7 @@ class Metadata:
         "/": { "name": "Home" },
         "/cv": { "name": "CV" },
         # "/reading": { "name": "Reading" },
-        "/projects": { "name": "Projects" },
+        # "/projects": { "name": "Projects" },
         "/writing": { "name": "Writing" },
         "https://linkedin.com/in/ntrupin/": {
             "name": "LinkedIn",
